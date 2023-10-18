6194d8c7163d1fc8bf638b9135040f9759a719b2
JeonYujeong7127 <yujj@konkuk.ac.kr>
2023-10-18 22:49:23 +0900

    Feat: Bullets

---
+++ Source/project/Bullet.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Bullet.generated.h"
+
+UCLASS()
+class PROJECT_API ABullet : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABullet();
+
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Projectile")
+	class USphereComponent* CollisionComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Movement")
+	class UProjectileMovementComponent* ProjectileMovementComponent;
+
+	FORCEINLINE class USphereComponent* GetProjectileCollision() const { return CollisionComponent; }
+	FORCEINLINE class UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovementComponent; }
+
+	UPROPERTY(VisibleAnyWhere, Category = "StaticMesh")
+	class UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY()
+		class UGameplayStatics* GameStatic;
+
+	UPROPERTY()
+		class UParticleSystem* FireParticle;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnHit(class UPrimitiveComponent* HitComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+};
